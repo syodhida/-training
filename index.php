b70e42d237077ebe18d24633f10d4445e1927183
RyotaAkiya <ryaii@nifty.com>
2018-04-11 13:30:53 +0900

    update php

--- index.php
+++ index.php
@@ -14,12 +14,18 @@ try {
 function main(){
     //templateを指定
     $template = './template/index.html';
+    $contents = html_output($template);
 
+    //指定した内容を出力
+    echo $contents;
+}
+
+function html_output($template){
     //htmlを取り込む
     ob_start();
     require $template;
     $contents = ob_get_clean();
 
-    //指定した内容を出力
-    echo $contents;
+    //出力内容を返す
+    return $contents;
 }
