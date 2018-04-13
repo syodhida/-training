1ba62052ab987fadc7f95de00d3e03f31946513e
yoshida <s.yoshida@coosy.co.jp>
2018-04-13 10:35:12 +0900

    ログイン途中まで

--- common/user_data.php
+++ common/user_data.php
@@ -6,17 +6,23 @@ class user_data {
         $params[] = [
             'ID'        => '1',
             'user_name' => '秋谷　亮太',
-            'job'       => 'programmer'
+            'job'       => 'programmer',
+            'email'     => 'test01@coosy.co.jp',
+            'password'  => 'coosy0101'
         ];
         $params[] = [
             'ID'        => '2',
             'user_name' => '吉田　忍',
-            'job'       => 'programmer'
+            'job'       => 'programmer',
+            'email'     => 'test02@coosy.co.jp',
+            'password'  => 'coosy0202'
         ];
         $params[] = [
             'ID'        => '3',
             'user_name' => '田中　裕也',
-            'job'       => 'lead programmer'
+            'job'       => 'lead programmer',
+            'email'     => 'test03@coosy.co.jp',
+            'password'  => 'coosy0303'
         ];
         return $params;
     }
--- index.php
+++ index.php
@@ -14,59 +14,24 @@ try {
 }
 
 function main(){
+
+
+    //クッキーからログイン状態を判別する
+    //↓クッキーの取得例
+    $email = '';
+    if(isset($_COOKIE["EMAIL"]))$email = $_COOKIE["EMAIL"];
+    //issetで値の有無を確認し、有る場合のみ代入する処理
+
+    $params['login_flag'] = 0;
     /*
-    $params = array();
-    $params[] = [
-        'ID'        => '1',
-        'user_name' => '秋谷　亮太',
-        'job'       => 'programmer'
-    ];
-    $params[] = [
-        'ID'        => '2',
-        'user_name' => '吉田　忍',
-        'job'       => 'programmer'
-    ];
-    $params[] = [
-        'ID'        => '3',
-        'user_name' => '田中　裕也',
-        'job'       => 'lead programmer'
-    ];
+    if(もしログイン中なら){
+        $params['login_flag'] = 1;
+    }
     */
-    
-    //var_dump($_GET);
-
-    //GETにてIDの取得
-    $id = $_GET['ID'];
-
-    //ユーザデータ取得
-    $user_data = user_data::get_userdata();
-    $output_user = [];
-    //------------------------------------------------
-    //ここで絞り込む
-
-        foreach ($user_data as $key => $value) {
-
-            if($id == $value['ID']){
-        
-		$output_user[] = [
-			 'ID' => $value['ID'],
-			 'user_name' => $value['user_name'],
-			 'job' => $value['job']
-		];
-    	    }
-    	}
-
-
-    //------------------------------------------------
-    //セット
-    $params = $output_user;
-
 
     //templateを指定
     $template = './template/index.html';
     $contents = common::html_output($template,$params);
-    //$contents = html_output($template,$params);
-
 
     //指定した内容を出力
     echo $contents;
